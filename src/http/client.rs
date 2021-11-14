71bb40e83fad22e96bec329f8638c85fd27a833d
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-13 21:11:39 -0800

    Fix errors

--- src/http/client.rs
+++ src/http/client.rs
@@ -17,7 +17,10 @@ use tokio::sync::Mutex;
 impl HttpClient {
     pub fn new(token: &str) -> Self {
         let mut headers = header::HeaderMap::new();
-        headers.insert("Authorization", header::HeaderValue::from_str(token));
+        headers.insert(
+            "Authorization",
+            header::HeaderValue::from_str(token).unwrap(),
+        ); // It shouldn't be Err but if it is for whatever reasson, it panic.
 
         let client_builder = configure_tls(ClientBuilder::new())
             .user_agent(format!("ferrischat.rs/{}", env!("CARGO_PKG_VERSION")))
@@ -25,7 +28,7 @@ impl HttpClient {
 
         let built_client = client_builder.build().expect("Failed to build http client");
 
-        let http = HttpClient {
+        HttpClient {
             client: Arc::new(built_client),
             token: token.to_string(),
             base_url: "https://api.ferris.chat/v0".to_string(),
@@ -35,7 +38,11 @@ impl HttpClient {
         // add_locks(&http);
     }
 
-    pub async fn request(&self, expected: u16, &request: &Request<'_>) -> Result<ReqwestResponse, Err> {
+    pub async fn request(
+        &self,
+        expected: u16,
+        &request: &Request<'_>,
+    ) -> Result<ReqwestResponse, Err> {
         for tries in 0..3 {
             let request_builder = self.client.request(
                 request.method,
@@ -43,26 +50,26 @@ impl HttpClient {
             );
 
             if let body = Some(request.body) {
-                request_builder.json(request.body);
+                request_builder.json(&body);
             }
 
             if let headers = Some(request.headers) {
-                request_builder.headers(request.headers);
+                request_builder.headers(headers);
             }
 
             let response = request_builder.send().await;
 
             if let Ok(response) = response {
                 if response.status().as_u16() == expected {
-                    return Ok(response)
+                    return Ok(response);
                 } else {
                     if tries == 2 {
-                        break
+                        break;
                     }
                 }
             } else {
                 if tries == 2 {
-                    break
+                    break;
                 }
             }
         }
