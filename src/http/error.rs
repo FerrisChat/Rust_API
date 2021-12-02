f6ac58540ed97addc5889d59b1cb106613cc069b
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-12-01 17:57:34 -0800

    Return string

--- src/http/error.rs
+++ src/http/error.rs
@@ -1,36 +1,19 @@
-use reqwest::{Error as ReqwestError, Response as ReqwestResponse, StatusCode, Url};
-
-#[derive(Clone, Debug, Serialize, PartialEq)]
-struct JsonDecodeError {
-    pub response_text: String,
-}
-
+use reqwest::{Response as ReqwestResponse, StatusCode, Url};
 
 #[derive(Clone, Debug, PartialEq)]
 struct HttpError {
     pub status_code: StatusCode,
     pub url: Url,
-    pub error: ReqwestError,
+    pub error: String,
 }
 
 impl HttpError {
     pub async fn from_response(r: ReqwestResponse) -> Self {
-        let mut error = r.json().await;
 
-        match error {
-            Ok(err) => {
-                error = err;
-            }
-            Err(_) => {
-                error = JsonDecodeError {
-                    response_text: r.text().await.unwrap_or_else(|| String::new()),
-                };
-            }
-        }
         Self {
             status_code: r.status(),
             url: r.url().clone(),
-            error,
+            r.text().await.unwrap_or_else(|_| String::new()),
         }
     }
 }
